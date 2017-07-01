8b1364f290e64ae7bbdee28c261ee824102e9580
soil <sh.yoshida@live.jp>
2017-07-01 17:14:40 +0900

    init

---
+++ lib/first_elixir.ex
@@ -0,0 +1,18 @@
+defmodule FirstElixir do
+  @moduledoc """
+  Documentation for FirstElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FirstElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
